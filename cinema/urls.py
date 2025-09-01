3d1270152be2af3151e9908ccfec05dd80e19aad
Serhii Basok <sergeybsk19@gmail.com>
2025-09-01 17:23:15 +0300

    create different api views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,24 @@
-from django.urls import path
+from django.urls import path, include
+from cinema.views import (MovieViewSet,
+                          GenreList,
+                          GenreDetail,
+                          ActorDetail,
+                          ActorList,
+                          CinemaHallViewSet,
+                          )
+from rest_framework import routers
 
-from cinema.views import movie_list, movie_detail
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+router.register("cinema_halls", CinemaHallViewSet)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("", include(router.urls)),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actors-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actors-detail"),
 ]
 
 app_name = "cinema"
